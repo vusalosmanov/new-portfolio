edb1f4a7aeadbba70e19180cb9ecafed8eba0c9f
Vusal Osmanov <115807599+vusalosmanov@users.noreply.github.com>
2023-12-17 18:40:09 +0400

    add data information and Skills , Projects

---
+++ src/data/Intormation.js
@@ -0,0 +1,46 @@
+export const information = {
+  social: {
+    github: "https://github.com/vusalosmanov",
+    linkedin: "linkedin.com/in/vusal-osmanov-720814268",
+    whatsapp: "https://wa.me/+9940559605850",
+    mail: "mailto:vusal.osmanov66@gmail.com",
+  },
+  home: {
+    title: "Hi, I'm Vusal Osmanov",
+    animationTitle: "frontend developer",
+    text: "I am developing myself in the field of programming and Front-End development. I have excellent knowledge and professional experience in this field and I continue to learn new technologies",
+  },
+  aboutText:
+    "I have been developing myself in the field of Frontend Developer for more than 1 year now and I have experience with many technologies, I am improving myself in this field. I have experience in developing powerful websites that meet client objectives. Able to identify needs, create plans, and work with clients to optimize design usability and performance. I can work effectively without supervision and learn new skills quickly. I develop responsive web pages for all devices",
+  // cv: "/personal/AskerovAlipashaCV.pdf",
+  // image: "/personal/image-2.png",
+  experience: [
+    {
+      id: 1,
+      position: "Frontend Developer Intern",
+      company: "Proton MMC",
+      location: "Nerimanov, Baku",
+      start: "09/2023",
+      end: "11/2023",
+    },
+  ],
+  education: [
+    {
+      id: 1,
+      univercity: "Azerbaijan State University of Economics • UNEC",
+      faculty: "Computer and Information Sciences and Support Services",
+    //   field: "Process Automation Engineering",
+    //   degree: "Bachelor Degree",
+      start: "2022",
+      end: "2024",
+    },
+    {
+      id: 2,
+      univercity: "Front end DeveloperTexnoera Academy",
+      faculty: "Web site development and learning programs",
+      degree: "Diploma",
+      start: "2022",
+      end: "2023",
+    },
+  ],
+};
---
+++ src/data/Projects.js
@@ -0,0 +1,22 @@
+export const projectsData = [
+    {
+        id: 1,
+        title: "Lucky",
+        image: "",
+        previewStatus: true,
+        githubStatus: true,
+        // previewLink: "",
+        githubLink: "https://github.com/vusalosmanov/LuckyApp",
+        skills: ['React JS', 'Redux', 'React Router', 'SCSS', 'Tailwind css'],
+    },
+    {
+        id: 2,
+        title: "MC Gallery",
+        image: "",
+        previewStatus: true,
+        githubStatus: true,
+        // previewLink: "",
+        githubLink: "https://github.com/vusalosmanov/MC-Gallery",
+        skills: ['React JS', 'Redux', 'React Router', 'SCSS', 'Tailwind css'],
+    },
+]
